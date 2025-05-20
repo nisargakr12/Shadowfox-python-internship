7f82a8f0e4eef1107f62cbbc9962014fbeca506a
unknown <nisargakratakalle@gmail.com>
2025-05-20 16:56:25 +0530

    Variables

---
+++ BeginnerLevel/Variables/variables.py
@@ -0,0 +1,23 @@
+#1. Store 22/7 in a variable called pi and check its type
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+
+#2. Create a variable called for and assign it a value 4.
+
+for = 4
+print(for)
+
+Output- It is a syntax error
+Reason- 'for' is a reserved keyword in Python. You cannot use it as a variable name.
+
+
+#3. Calculate Simple Interest
+principal = 5000    
+rate = 5          
+time = 3           
+
+simple_interest = (principal * rate * time) / 100
+print(f"simple_interest =",simple_interest)
+
